c4cccf834c98013b257f3d73f1855053ebfe2665
Colin Gleason <colingleason805@gmail.com>
2024-12-07 20:30:23 -0800

    day 3 part 2

--- src/3/src/day_3.rs
+++ src/3/src/day_3.rs
@@ -20,13 +20,44 @@ pub fn dowork() {
                 .filter_map(|nums| nums.as_str().parse::<i32>().ok())
                 .collect();
 
-            sum+= vals[0] * vals[1];
+            sum += vals[0] * vals[1];
         }
     }
     println!("sum is {sum}")
 }
 
-pub fn dowork2() {}
+pub fn dowork2() {
+    let re = Regex::new("do[(][)]|don't[(][)]|mul[(][0-9]+,[0-9]+[)]").unwrap();
+    let mul_vals = Regex::new("[0-9]+").unwrap();
+    let do_vals = Regex::new("do[(][)]").unwrap();
+    let dont_vals = Regex::new("don't[(][)]").unwrap();
+    let mut sum = 0;
+    let mut do_mul = true;
+
+    for s in get_data() {
+        let instr: Vec<&str> = re.find_iter(&s).map(|m| m.as_str()).collect();
+
+        for mul in instr {
+            if do_vals.is_match(mul) {
+                do_mul = true;
+                continue;
+            } else if dont_vals.is_match(mul) {
+                do_mul = false;
+                continue;
+            }
+
+            if do_mul {
+                let vals: Vec<i32> = mul_vals
+                    .find_iter(mul)
+                    .filter_map(|nums| nums.as_str().parse::<i32>().ok())
+                    .collect();
+
+                sum += vals[0] * vals[1];
+            }
+        }
+    }
+    println!("sum2 is {sum}")
+}
 
 fn get_data() -> Vec<String> {
     let f = File::open("C://workspaces//aoc2024//src//3//src//input.txt").unwrap();
