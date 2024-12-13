8e1f14ed679a3827dc185d1c6e7213ef76bac905
Colin Gleason <colingleason805@gmail.com>
2024-12-13 13:33:52 -0800

    day 4 refactor

--- src/day_4/src/day_4.rs
+++ src/day_4/src/day_4.rs
@@ -23,178 +23,83 @@ pub fn dowork() {
 fn search(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
     let mut found = 0;
 
-    found += up(y, x, c, &pzl);
-    found += down(y, x, c, &pzl);
-    found += right(y, x, c, &pzl);
-    found += left(y, x, c, &pzl);
-    found += down_l(y, x, c, &pzl);
-    found += down_r(y, x, c, &pzl);
-    found += up_l(y, x, c, &pzl);
-    found += up_r(y, x, c, &pzl);
+    found += search_cardinal(y, x, c, "u", &pzl);
+    found += search_cardinal(y, x, c, "d", &pzl);
+    found += search_cardinal(y, x, c, "r", &pzl);
+    found += search_cardinal(y, x, c, "l", &pzl);
+    found += search_cardinal(y, x, c, "d_l", &pzl);
+    found += search_cardinal(y, x, c, "d_r", &pzl);
+    found += search_cardinal(y, x, c, "u_l", &pzl);
+    found += search_cardinal(y, x, c, "u_r", &pzl);
 
     return found;
 }
 
-fn search_cardinal(y: usize, x: usize, c: char, cardinality: &str, pzl: &Vec<Vec<char>>){
+fn search_cardinal(y: usize, x: usize, c: char, cardinality: &str, pzl: &Vec<Vec<char>>) -> i32 {
     let y_add = get_y_add_for_cardinality(cardinality);
     let x_add = get_x_add_for_cardinality(cardinality);
 
-    // if y <= 0 {
-    //     return 0;
-    // }
+    if y >= pzl.len() || x >= pzl[y].len() {
+        return 0;
+    }
 
-    // if char_at_equals(y - 1, x, c, pzl) {
-    //     if c == 'S' {
-    //         return 1;
-    //     } else {
-    //         return up(y - 1, x, next_char(c), pzl);
-    //     }
-    // } else {
-    //     return 0;
-    // }
-}
-
-fn get_y_add_for_cardinality(cardinality: &str) -> i32{
-    match cardinality{
-        "u" | "u_l" | "u_r"=> return -1,
-        "d" | "d_l" | "d_r" => return 1,
-        "l"| "r" => return 0,
-        _ => return i32::MIN
+    if char_at_equals(y, x, c, pzl) {
+        if c == 'S' {
+            return 1;
+        } else {
+            // don't go out of bounds
+            if !check_bounds(y, x, cardinality, pzl) {
+                return 0;
+            }
+            return search_cardinal(
+                (y as i32 + y_add) as usize,
+                (x as i32 + x_add) as usize,
+                next_char(c),
+                cardinality,
+                pzl,
+            );
+        }
+    } else {
+        return 0;
     }
 }
 
-fn get_x_add_for_cardinality(cardinality: &str) -> i32{
-    match cardinality{
+fn check_bounds(y: usize, x: usize, cardinality: &str, pzl: &Vec<Vec<char>>) -> bool {
+    if y == 0 && get_y_add_for_cardinality(cardinality) == -1 {
+        return false;
+    } else if y == pzl.len() - 1 && get_y_add_for_cardinality(cardinality) == 1 {
+        return false;
+    } else if x == 0 && get_x_add_for_cardinality(cardinality) == -1 {
+        return false;
+    } else if x == pzl[y].len() - 1 && get_x_add_for_cardinality(cardinality) == 1 {
+        return false;
+    }
+    return true;
+}
+
+fn get_y_add_for_cardinality(cardinality: &str) -> i32 {
+    match cardinality {
+        "u" | "u_l" | "u_r" => return -1,
+        "d" | "d_l" | "d_r" => return 1,
+        "l" | "r" => return 0,
+        _ => return i32::MIN,
+    }
+}
+
+fn get_x_add_for_cardinality(cardinality: &str) -> i32 {
+    match cardinality {
         "l" | "u_l" | "d_l" => return -1,
         "r" | "u_r" | "d_r" => return 1,
-        "u"| "d" => return 0,
-        _ => return i32::MIN
+        "u" | "d" => return 0,
+        _ => return i32::MIN,
     }
 }
 
-fn up(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
-    if y <= 0 {
-        return 0;
+fn char_at_equals(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> bool {
+    if y >= pzl.len() || x >= pzl[y].len() {
+        println!("uh oh");
     }
-
-    if char_at_equals(y - 1, x, c, pzl) {
-        if c == 'S' {
-            return 1;
-        } else {
-            return up(y - 1, x, next_char(c), pzl);
-        }
-    } else {
-        return 0;
-    }
-}
-fn down(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
-    if y <= pzl.len() {
-        return 0;
-    }
-
-    if char_at_equals(y + 1, x, c, pzl) {
-        if c == 'S' {
-            return 1;
-        } else {
-            return down(y + 1, x, next_char(c), pzl);
-        }
-    } else {
-        return 0;
-    }
-}
-fn left(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
-    if x <= 0 {
-        return 0;
-    }
-
-    if char_at_equals(y, x - 1, c, pzl) {
-        if c == 'S' {
-            return 1;
-        } else {
-            return left(y, x - 1, next_char(c), pzl);
-        }
-    } else {
-        return 0;
-    }
-}
-fn right(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
-    if x >= pzl[y].len() - 1 {
-        return 0;
-    }
-
-    if char_at_equals(y, x + 1, c, pzl) {
-        if c == 'S' {
-            return 1;
-        } else {
-            return right(y, x + 1, next_char(c), pzl);
-        }
-    } else {
-        return 0;
-    }
-}
-fn up_l(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
-    if y <= 0 || x <= 0 {
-        return 0;
-    }
-
-    if char_at_equals(y - 1, x - 1, c, pzl) {
-        if c == 'S' {
-            return 1;
-        } else {
-            return up_l(y - 1, x - 1, next_char(c), pzl);
-        }
-    } else {
-        return 0;
-    }
-}
-fn up_r(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
-    if y <= 0 || x > pzl[y].len() - 1 {
-        return 0;
-    }
-
-    if char_at_equals(y - 1, x + 1, c, pzl) {
-        if c == 'S' {
-            return 1;
-        } else {
-            return up_r(y - 1, x + 1, next_char(c), pzl);
-        }
-    } else {
-        return 0;
-    }
-}
-fn down_l(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
-    if y > pzl.len() - 1 || x <= 0 {
-        return 0;
-    }
-
-    if char_at_equals(y + 1, x - 1, c, pzl) {
-        if c == 'S' {
-            return 1;
-        } else {
-            return up(y + 1, x - 1, next_char(c), pzl);
-        }
-    } else {
-        return 0;
-    }
-}
-fn down_r(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
-    if y > pzl.len() - 1 ||  x > pzl[y].len() {
-        return 0;
-    }
-
-    if char_at_equals(y + 1, x+1, c, pzl) {
-        if c == 'S' {
-            return 1;
-        } else {
-            return up(y + 1, x+1, next_char(c), pzl);
-        }
-    } else {
-        return 0;
-    }
-}
-
-fn char_at_equals(i: usize, j: usize, c: char, pzl: &Vec<Vec<char>>) -> bool {
-    return pzl[i][j] == c;
+    return pzl[y][x] == c;
 }
 
 fn next_char(c: char) -> (char) {
@@ -226,3 +131,125 @@ fn get_data() -> Vec<Vec<char>> {
     }
     return pzl;
 }
+
+fn up(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
+    if y <= 0 {
+        return 0;
+    }
+
+    if char_at_equals(y, x, c, pzl) {
+        if c == 'S' {
+            return 1;
+        } else {
+            return up(y - 1, x, next_char(c), pzl);
+        }
+    } else {
+        return 0;
+    }
+}
+fn down(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
+    if y <= pzl.len() {
+        return 0;
+    }
+
+    if char_at_equals(y, x, c, pzl) {
+        if c == 'S' {
+            return 1;
+        } else {
+            return down(y + 1, x, next_char(c), pzl);
+        }
+    } else {
+        return 0;
+    }
+}
+fn left(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
+    if x <= 0 {
+        return 0;
+    }
+
+    if char_at_equals(y, x, c, pzl) {
+        if c == 'S' {
+            return 1;
+        } else {
+            return left(y, x - 1, next_char(c), pzl);
+        }
+    } else {
+        return 0;
+    }
+}
+fn right(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
+    if x >= pzl[y].len() {
+        return 0;
+    }
+
+    if char_at_equals(y, x, c, pzl) {
+        if c == 'S' {
+            return 1;
+        } else {
+            return right(y, x + 1, next_char(c), pzl);
+        }
+    } else {
+        return 0;
+    }
+}
+fn up_l(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
+    if y <= 0 || x <= 0 {
+        return 0;
+    }
+
+    if char_at_equals(y, x, c, pzl) {
+        if c == 'S' {
+            return 1;
+        } else {
+            return up_l(y - 1, x - 1, next_char(c), pzl);
+        }
+    } else {
+        return 0;
+    }
+}
+fn up_r(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
+    if y <= 0 || x >= pzl[y].len() {
+        return 0;
+    }
+
+    if char_at_equals(y, x, c, pzl) {
+        if c == 'S' {
+            return 1;
+        } else {
+            return up_r(y - 1, x + 1, next_char(c), pzl);
+        }
+    } else {
+        return 0;
+    }
+}
+fn down_l(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
+    if y >= pzl.len() || x <= 0 {
+        return 0;
+    }
+
+    if char_at_equals(y, x, c, pzl) {
+        if c == 'S' {
+            return 1;
+        } else {
+            return up(y + 1, x - 1, next_char(c), pzl);
+        }
+    } else {
+        return 0;
+    }
+}
+fn down_r(y: usize, x: usize, c: char, pzl: &Vec<Vec<char>>) -> i32 {
+    if y >= pzl.len() || x >= pzl[y].len() {
+        return 0;
+    }
+
+    if char_at_equals(y, x, c, pzl) {
+        if c == 'S' {
+            return 1;
+        } else {
+            return up(y + 1, x + 1, next_char(c), pzl);
+        }
+    } else {
+        return 0;
+    }
+}
+
